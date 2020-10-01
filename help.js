5789c85312921a8202954c547c27fd3ce52eb7c5
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2020-10-01 16:45:56 -0400

    Improving features

--- help.js
+++ help.js
@@ -1,16 +1,11 @@
-function fun1(){
-  document.write(5 + 6);
-  var visible = false;
-  var t = document.getElementById("position").value;
-  document.write(t);
-  if (t.equals("Elderly")){
-    visible = false;
-  }
-  if (t.equals("Volunteer")){
-    visible = true;
-  }
-
-  if(visible){
-    document.write(5 + 6);
-  }
+function Elder(){
+  window.open("https://www.w3schools.com");
+}
+function Volunteer(){
+  window.open("https://www.messenger.com/t/3355852651136126");
+}
+function Addlist(){
+  var hold = document.getElementById("Task").value;
+  var t = document.createTextNode(hold);
+  document.body.appendChild(t);
 }
