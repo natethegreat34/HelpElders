5980719abc8323039f7dc277587970fac8b2c0f2
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2020-10-03 15:34:59 -0400

    Added more precautions

--- help.js
+++ help.js
@@ -1,24 +1,26 @@
 window.addEventListener('load', function() { 
   console.log('All assets are loaded') 
   document.getElementById("makingTask").onclick = function(){
-    event.preventDefault()
+    if (confirm("Are you sure you would like to delete the task?")){
+      event.preventDefault()
 
-    var newTask = {
-      "name" : document.getElementById("taskName").value,
-      "user" : document.getElementById("name").value,
-      "helpneeded": document.getElementById("address").value
+      var newTask = {
+        "name" : document.getElementById("taskName").value,
+        "user" : document.getElementById("name").value,
+        "helpneeded": document.getElementById("address").value
+      }
+      // fetch("https://6rvpnnte.brev.dev/api/Form"+"?id=uuuuiiiiddddd", {method: "DELETE"})
+      fetch("https://6rvpnnte.brev.dev/api/Form", {
+        method: "POST",
+        body:JSON.stringify(newTask)
+      } );
+      fetch("https://6rvpnnte.brev.dev/api/Form").then(response => response.json())
+  .then(data => {
+    console.log('Success:', data);
+    data.forEach(elem => {console.log(elem)})
+  })
+
+      console.log("I've been clicked")
     }
-    // fetch("https://6rvpnnte.brev.dev/api/Form"+"?id=uuuuiiiiddddd", {method: "DELETE"})
-    fetch("https://6rvpnnte.brev.dev/api/Form", {
-      method: "POST",
-      body:JSON.stringify(newTask)
-    } );
-    fetch("https://6rvpnnte.brev.dev/api/Form").then(response => response.json())
-.then(data => {
-  console.log('Success:', data);
-  data.forEach(elem => {console.log(elem)})
-})
-
-    console.log("I've been clicked")
-  }
+}
 })
