2afa727af8804452f70edf0fbf5d5400bf703ba1
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2020-10-03 10:35:55 -0400

    visualizing ideas

--- help.js
+++ help.js
@@ -8,8 +8,11 @@ window.addEventListener('load', function() { 
       "user" : document.getElementById("name").value,
       "helpneeded": document.getElementById("address").value
     }
-
-    fetch("https://6rvpnnte.brev.dev/api/Form", {method: "POST", body:JSON.stringify(newTask)} )
+    // fetch("https://6rvpnnte.brev.dev/api/Form"+"?id=uuuuiiiiddddd", {method: "DELETE"})
+    fetch("https://6rvpnnte.brev.dev/api/Form", {
+      method: "POST",
+      body:JSON.stringify(newTask)
+    } );
     fetch("https://6rvpnnte.brev.dev/api/Form").then(response => response.json())
 .then(data => {
   console.log('Success:', data);
@@ -19,16 +22,3 @@ window.addEventListener('load', function() { 
     console.log("I've been clicked")
   }
 })
-
-  // <div class="container">
-  //   <label for="taskName" class="sr-only">Help Needed</label>
-  //   <input type="text" id="taskName" class="form-control" placeholder="Task Name" required autofocus>
-  //   <br></br>
-  //   <label for="name" class="sr-only">Name</label>
-  //   <input type="text" id="name" class="form-control" placeholder="Name" required>
-  //   <br></br>
-  //   <label for="address" class="sr-only">Address</label>
-  //   <input type="text" id="address" class="form-control" placeholder="Address" required>
-  //   <br></br>
-  //   <button class="btn btn-lg btn-primary btn-block" type="button" id="makingTask">Create Task</button>
-  // </div>
---
+++ lisp.js
@@ -0,0 +1,6 @@
+function123(){
+  var node = document.createElement("LI");
+  var textnode = document.createTextNode("Water");
+  node.appendChild(textnode);
+  document.getElementById("myList").appendChild(node);
+}
