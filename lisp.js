1040bfd8abbf3c856ca0b93a9bf837461c1405d7
natethegreat34 <43049313+natethegreat34@users.noreply.github.com>
2020-10-03 15:13:14 -0400

    All basics done

--- lisp.js
+++ lisp.js
@@ -1,6 +1,25 @@
-function123(){
-  var node = document.createElement("LI");
-  var textnode = document.createTextNode("Water");
-  node.appendChild(textnode);
-  document.getElementById("myList").appendChild(node);
-}
+window.addEventListener('load', function(){
+  fetch("https://6rvpnnte.brev.dev/api/Form").then(response => response.json())
+  .then(data => {
+  console.log('Success:', data);
+    Object.keys(data).forEach(key=> {
+    var task = data[key]
+    var node = document.createElement("LI");
+    node.onclick = function(){
+      console.log(task.name)
+      console.log(key)
+      fetch("https://6rvpnnte.brev.dev/api/Form"+ "?id="+key , {method: "DELETE"})
+      node.style.display="none"
+    }
+    var textnode = document.createTextNode("| Task: " + task.name + " | Name: " + task.user + " | Address: " + task.helpneeded+ " |");
+    node.appendChild(textnode);
+    document.getElementById("myList").appendChild(node)
+    // var button = document.createElement("button");
+    //  document.getElementById("myList").appendChild(button);
+    //  button.addEventListener ("click", function() {
+    //  document.getElementById("myList").removeChild(node);
+    //  document.getElementById("myList").removeChild(button);
+  })
+  })
+
+})
